c3b1b99d549e695d7086ad74c658ac68b553e46b
Suematsu <zap02570@hotmail.com>
2015-12-19 11:56:24 +0900

    third

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class Hello{
 	public static void main(String args[]){
+		System.out.println("Hello　GitHub World2！！");//3回目、これを追加してみた
 		System.out.println("Hello　GitHub World2！！");//2回目、これを追加してみた
 		System.out.println("Hello　GitHub World！！");
 	}
